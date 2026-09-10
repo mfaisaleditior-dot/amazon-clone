e799a10a49e6d54dbba77386875cf1b608aba2f6
mfaisaleditior-dot <mfaisaleditior@gmail.com>
2026-09-10 08:01:02 +0500

    Add files via upload

---
+++ faisal.js
@@ -0,0 +1,48 @@
+function greater(a,b){
+    if(a>=50){
+        return pass;
+
+    }if else{
+        
+    }
+}
+let result = greater (75,50);
+console.log(result);
+
+
+function age(age){
+    if (age>= 18){
+        return "adult";
+    } else{
+        return "not adult";
+    }
+}
+
+let result = age(12);
+console.log(result);
+
+
+function calculate(price,quailty){
+    return price*quailty;
+}
+
+ let result = calculate(100,3);
+ console.log(result);
+
+let name = "faisal";
+
+function greet(){
+    console.log(name);
+}
+ greet();
+
+ let age = 17;
+
+ function age(){
+    console.log (age);
+ }
+
+
+
+ 
+
